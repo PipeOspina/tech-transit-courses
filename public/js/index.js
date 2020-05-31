bf86f26cfe26b359b10604329be83718e2b1dfdc
Felipe Ospina <pipe.98.oh@gmail.com>
2020-05-30 22:12:11 -0500

    Finishing with frontend All features like styles, functions and all of user interface, following requirements from oficial document.

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,52 @@ const COURSE_PRICE_CLASS = 'course-price'
 const COURSE_CREDITS_CLASS = 'course-credits'
 const COURSE_RATING_CLASS = 'course-rating'
 
-const buildCards = async () => {
-    for await (const course of window._courses) {
+const container = document.getElementById('courses-container')
+const baseCardCourse = document.getElementById('course-0000')
+
+const creditsSlide = document.getElementById('credits')
+const starsSlide = document.getElementById('stars')
+const priceSlide = document.getElementById('price')
+
+const searchInput = document.getElementById('search')
+
+const footerLabel = document.getElementById('footer-label')
+
+const credits = window._courses.map(course => {
+    return course.maximumCredits
+})
+const stars = window._courses.map(course => {
+    return course.rating
+})
+const prices = window._courses.map(course => {
+    return course.price
+})
+
+let currentCourses = window._courses
+
+let filters = {
+    credits: 0,
+    stars: 0,
+    price: 0,
+    search: ''
+}
+
+const start = () => {
+    creditsSlide.max = Math.max(...credits) + 1
+    starsSlide.max = Math.max(...stars)
+    priceSlide.max = Math.max(...prices)
+    creditsSlide.min = Math.min(...credits)
+    starsSlide.min = Math.min(...stars)
+    priceSlide.min = Math.min(...prices)
+    creditsSlide.value = 0
+    starsSlide.value = 0
+    priceSlide.value = 0
+    addRatingStars()
+    buildCards(currentCourses)
+}
+
+const buildCards = async (elements) => {
+    for await (const course of elements) {
         const copy = baseCardCourse.cloneNode(true)
         copy.id = course.id.toString().padStart(11, 'course-0000')
         for await (const element of copy.childNodes) {
@@ -14,7 +58,6 @@ const buildCards = async () => {
         }
         container.appendChild(copy);
     }
-    baseCardCourse.remove()
 }
 
 const strip = (html) => {
@@ -23,7 +66,6 @@ const strip = (html) => {
  }
 
 const switchCardClassName = (element, course) => {
-    // element = document.getElementById('courses-container')
     switch(element.className) {
         case COURSE_IMAGE_CLASS:
             element.src = `https://techtransit.com/mission.courses${course.imageUrl}`
@@ -44,14 +86,54 @@ const switchCardClassName = (element, course) => {
         case COURSE_RATING_CLASS:
             element.childNodes[3].style.width = `${course.rating * 100 / 5}%`
             element.title = course.rating.toString()
+            applyRatingColor(element, course.rating)
             break
         default:
             break
     }
 }
 
-const container = document.getElementById('courses-container')
-const baseCardCourse = document.getElementById('course-0000')
+const applyRatingColor = async (element, rating) => {
+    const emptyStars = await element.childNodes[1].childNodes
+    const fullStars = await element.childNodes[3].childNodes
+    emptyStars.forEach((star, index) => {
+        star.style.fill = switchRatingColors(rating)
+        fullStars[index].style.fill = switchRatingColors(rating)
+        if(rating === 5) {
+            let size;
+            if(index === 0 || index === 4) size = '1rem'
+            else if(index === 1 || index === 3) size = '2rem'
+            else size = '3rem'
+            star.style.width = size
+            star.style.height = size
+            fullStars[index].style.width = size
+            fullStars[index].style.height = size
+            element.childNodes[1].style.display = 'flex'
+            element.childNodes[3].style.display = 'flex'
+            element.childNodes[1].style.alignItems = 'center'
+            element.childNodes[3].style.alignItems = 'center'
+        }
+    })
+}
+
+const switchRatingColors = (rating) => {
+    switch(true) {
+        case (rating === 0):
+            return 'darkred'
+        case (rating > 0 && rating <=  1):
+            return 'crimson'
+        case (rating > 1 && rating <= 2):
+            return 'darkorange'
+        case (rating > 2 && rating <= 3):
+            return 'orange'
+        case (rating > 3 && rating <= 4):
+            return 'greenyellow'
+        case (rating > 4 && rating < 5):
+            return 'forestgreen'
+        default:
+            return 'gold'
+    }
+}
 
 const addRatingStars = () => {
     const baseRating = baseCardCourse.childNodes[11]
@@ -67,9 +149,53 @@ const addRatingStars = () => {
         fullStars.appendChild(fullStarCopy)
     }
     
+    emptyStars.childNodes[0].remove()
+    emptyStars.childNodes[1].remove()
+    fullStars.childNodes[0].remove()
+    fullStars.childNodes[1].remove()
+
     emptyStar.remove()
     fullStar.remove()
 }
-    
-addRatingStars()
-buildCards()
+
+const applyFilter = (event) => {
+    const value = !isNaN(parseInt(event.target.value)) ? parseInt(event.target.value) : event.target.value
+    filters = {
+        ...filters,
+        [event.target.id]: value
+    }
+    console.log(filters)
+    const allCourses = window._courses
+    const filteredCourses = allCourses.filter((course) => {
+        return (
+            course.maximumCredits >= filters.credits &&
+            course.rating >= filters.stars &&
+            course.price >= filters.price &&
+            course.name.toLowerCase().includes(filters.search)
+        )
+    })
+    if(event.target.id !== 'search') {
+        document.getElementById(`${event.target.id}-label`).textContent = value === 0 ? 'All' : `${value} at least`
+    }
+    currentCourses = filteredCourses
+    footerLabel.textContent = filteredCourses.length === window._courses.length ? `Showing all courses (${window._courses.length})` : `Showing ${filteredCourses.length} of ${window._courses.length} courses`
+    updateDOM()
+}
+
+const updateDOM = () => {
+    const ids = currentCourses.map(course => {
+        return course.id.toString().padStart(11, 'course-0000')
+    })
+    for (const child of container.childNodes) {
+        if(child.tagName === 'SECTION') {
+            child.style.display = ids.includes(child.id) ? 'grid' : 'none'
+        }
+    }
+}
+
+creditsSlide.addEventListener('input', applyFilter)
+starsSlide.addEventListener('input', applyFilter)
+priceSlide.addEventListener('input', applyFilter)
+searchInput.addEventListener('input', applyFilter)
+
+start()
